2ef57ba30f4666dcc34e83e5cd5c8032d7750625
zettasim15 <simmdm15@gmail.com>
2025-10-15 14:24:22 +0700

    Finalizing setup: Add .gitignore and correct requirements

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\PBL SMT 3\prediksi serangan hama agroguard\venv
+set VIRTUAL_ENV=C:\Users\IDEAPAD\prediksi-serangan-hama-agroguard\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
