86cd6ba3d968d1a0f4d66c64f88327a2c4ccaf48
Marsel <marselnbho@gmail.com>
2025-10-18 21:06:38 +0700

    Penghapusan halaman admin yang tergabung di admin dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-class PencegahanHama(models.Model):
-    jenis_hama = models.CharField(max_length=100)
-    judul = models.CharField(max_length=200)
-    sumber = models.CharField(max_length=200)
-    deskripsi = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.judul
